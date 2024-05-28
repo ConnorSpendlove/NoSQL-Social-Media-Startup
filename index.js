961559c4045d47a15fba2d86c1013548094932b5
unknown <Connor.Spendlove44@gmail.com>
2024-05-27 18:46:10 -0600

    set up server and made some route files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// mongoose connection
+mongoose.connect('mongodb://localhost:27017/nosqlSocialMediaStartup', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set('debug', true);
+
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
